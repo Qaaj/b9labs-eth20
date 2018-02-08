59c430b28bf1175ec6f29b3d3c36e0541ebd9a1a
Jan Jorissen <grasmaaier45@gmail.com>
2018-02-08 11:55:21 +0100

    First version of remittance

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ const CloseButton = styled.div`
 
 class App extends Component {
 
-  // state = { component: null };
-  state = { component: 'splitter' };
+  state = { component: null };
+  // state = { component: 'splitter' };
 
   constructor(props) {
     super(props);
