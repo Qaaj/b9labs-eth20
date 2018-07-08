0d2ae15fbba1c4ee42300b9d7cdf455eabace536
Jan Jorissen <grasmaaier45@gmail.com>
2018-07-08 14:18:14 +0200

    Sol 0.4.24

---
+++ contracts/Ownable.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.21;
+
+contract Ownable {
+    address public owner;
+
+	modifier only_owner {
+	    require(msg.sender == owner);
+	    _;
+	}
+
+	event LogNewOwner(address indexed old, address indexed current);
+
+    // Constructor
+    function ()
+    public {
+        owner = msg.sender;
+    }
+
+    function setOwner(address _new)
+    only_owner
+    public {
+        require(_new != owner);
+        require(_new != address(0));
+
+        emit LogNewOwner(owner, _new);
+
+        owner = _new;
+    }
+}
--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -1,49 +1,43 @@
-pragma solidity 0.4.18;
+pragma solidity 0.4.24;
 
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
-// Alice creates a new Remittance Instance with 2 passwords, ETH and a deadlineBlock and a benificiary
-// Carol trades Bob's password for the funds
-// Carol then unlock the funds with Bob's password, and her address
-// Alice can withdraw her funds after a certain point, with maxDeadlineHeight defining the maximum blockheight after the creation
-// Upon withdrawal, a fee is taken based on current tx.gasprice * contractCreationGasCost
-// The contract owner can withdraw the current bankroll with takeProfits()
+import './Ownable.sol';
 
 contract Remittance is Ownable {
-  
+
   event LogRemittanceClaimed(address claimer, bytes32 instanceId, uint amount);
   event LogRemittanceCreated(address owner, uint amount, bytes32 instanceId, uint fee);
   event LogRemittanceCancelled(address remittanceInstanceOwner, bytes32 instanceId);
   event LogKillswitch(address receiver, uint amount);
   event LogPaused(address sender, bool paused);
-  
+
   mapping (bytes32 => RemittanceInstance) public remittances;
-  
+
   // Current estimate of contract creation cost
   uint contractCreationGasCost = 2000000;
   // Maximum amount of blocks after which remittance owner can withdraw
   uint public maxDeadlineHeight = 1000;
   // Profit ready for withdrawal
   uint public bankroll;
-  
+
   bool paused;
   modifier pausable{
     require(!paused);
     _;
   }
-  
+
   struct RemittanceInstance {
     address owner;
     bytes32 passphrase; // Hashed password_one
     uint amount; // Amount of eth
     uint deadlineBlock; //  BlockHeight after which amount is reclaimable
   }
-  
-  function Remittance() public {
+
+  function () public {
     owner = msg.sender;
   }
-  
+
   // Person B can then claim the funds with both passwords
-  function claimFunds(bytes32 identifier, string password)
+  function claimFunds(bytes32 identifier, bytes password)
   pausable
   public
   {
@@ -52,9 +46,12 @@ contract Remittance is Ownable {
     // Make sure there is a instance under that name AND that there is still money to be sent
     require(instance.amount != 0);
     // We need the second hashed password to match plus the senders address
-    require(instance.passphrase == keccak256(msg.sender, keccak256(password)));
-    
-    LogRemittanceClaimed(msg.sender, identifier, instance.amount);
+    require(instance.passphrase == keccak256(
+      abi.encodePacked(msg.sender,keccak256(password))
+    )
+    );
+
+    emit LogRemittanceClaimed(msg.sender, identifier, instance.amount);
     // Save the amount to be sent in a seperate variable
     uint amountToSend = instance.amount;
     // Clear the instance amount
@@ -62,7 +59,7 @@ contract Remittance is Ownable {
     // Finally, transfer the amount to the person claiming it
     msg.sender.transfer(amountToSend);
   }
-  
+
   // Owner of a project can cancel the remittance up until a certain point
   function cancelProject(bytes32 identifier)
   public
@@ -73,7 +70,7 @@ contract Remittance is Ownable {
     require(instance.owner == msg.sender);
     // After the deadline block, owner can claim funds
     require(instance.deadlineBlock >= block.number);
-    LogRemittanceCancelled(instance.owner, identifier);
+    emit LogRemittanceCancelled(instance.owner, identifier);
     // Save the amount to be sent in a seperate variable
     uint amountToSend = instance.amount;
     // Clear the instance amount
@@ -81,7 +78,7 @@ contract Remittance is Ownable {
     // Finally, transfer the amount to the owner
     msg.sender.transfer(amountToSend);
   }
-  
+
   // Create a new Remittance project
   function newProject(bytes32 passphrase,  address claimer, uint deadlineBlock)
   public
@@ -94,7 +91,7 @@ contract Remittance is Ownable {
     // There needs to be something to remit
     require(msg.value > 0);
     // Create new Project struct
-    bytes32 finalHash = keccak256(claimer,passphrase);
+    bytes32 finalHash = keccak256(abi.encodePacked(claimer,passphrase));
     // Calculate current fee based on gasprice
     uint fee = tx.gasprice * 1000000 * contractCreationGasCost;
     // Add fee to the bankroll
@@ -102,7 +99,7 @@ contract Remittance is Ownable {
     // Gotta pay taxes
     uint amountToSend = msg.value - fee;
     require(amountToSend > 0);
-    
+
     RemittanceInstance memory remittanceInstance =
     RemittanceInstance(
       {owner: msg.sender,
@@ -110,30 +107,30 @@ contract Remittance is Ownable {
       amount: amountToSend,
       deadlineBlock: deadlineBlock
       });
-    
+
     remittances[passphrase] = remittanceInstance;
-    LogRemittanceCreated(msg.sender, amountToSend, passphrase, fee);
+    emit LogRemittanceCreated(msg.sender, amountToSend, passphrase, fee);
     return passphrase;
   }
-  
+
   // Emergency function to drain all funds from the contract and pauses it
   function panicButton(address emergencyExit)
   public
-  onlyOwner
+  only_owner
   {
     if(emergencyExit != address(0))
     {
-      LogKillswitch(emergencyExit, this.balance);
-      emergencyExit.transfer(this.balance);
+      emit LogKillswitch(emergencyExit, address(this).balance);
+      emergencyExit.transfer(address(this).balance);
     }else{
-      LogKillswitch(msg.sender, this.balance);
-      msg.sender.transfer(this.balance);
+      emit LogKillswitch(msg.sender, address(this).balance);
+      msg.sender.transfer(address(this).balance);
     }
   }
-  
+
   // Drain the bankroll
   function takeProfits()
-  onlyOwner
+  only_owner
   public
   {
     require(msg.sender == owner);
@@ -141,20 +138,20 @@ contract Remittance is Ownable {
     bankroll = 0;
     owner.transfer(toSend);
   }
-  
+
   function setPaused(bool value)
-  onlyOwner
+  only_owner
   public
   returns (bool isPaused)
   {
     require(value != paused);
     paused = value;
-    LogPaused(msg.sender, paused);
+    emit LogPaused(msg.sender, paused);
     return paused;
   }
-  
+
   // Helper functions
-  
+
   function blockHeight()
   public
   view
@@ -163,7 +160,7 @@ contract Remittance is Ownable {
     uint height = block.number;
     return height;
   }
-  
+
   function gasPrice()
   public
   view
@@ -171,13 +168,13 @@ contract Remittance is Ownable {
   {
     return (tx.gasprice * 1000000);
   }
-  
-  function hashPassword(bytes32 password)
+
+  function hashPassword(bytes password)
   public
   pure
   returns(bytes32 hash)
   {
     return keccak256(password);
   }
-  
+
 }
