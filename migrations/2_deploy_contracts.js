3ad82d36213f5b866cf02fb9095fa54d1ec0cb2b
Jan Jorissen <grasmaaier45@gmail.com>
2018-07-08 14:35:23 +0200

    New migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var YouTube = artifacts.require("./YouTube.sol");
+var Remittance = artifacts.require("./Remittance.sol");
+var Splitter = artifacts.require("./Splitter.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(YouTube);
+  deployer.deploy(Remittance);
+  deployer.deploy(Splitter);
 };
