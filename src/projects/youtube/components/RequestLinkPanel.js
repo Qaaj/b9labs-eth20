b89cfa574b7c83327f5db274ea1fd94f4e109d64
Jan Jorissen <grasmaaier45@gmail.com>
2018-07-08 13:08:55 +0200

    Blocky can't deal with undefined

--- src/projects/youtube/components/RequestLinkPanel.js
+++ src/projects/youtube/components/RequestLinkPanel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Form, Input, Label, Message, TextArea, Loader} from 'semantic-ui-react';
+import { Form, Input, Label, Message, TextArea, Loader } from 'semantic-ui-react';
 import styled from 'styled-components';
 import Blocky from './Blocky';
 import BaseModal from './modals/BaseModal';
@@ -32,11 +32,14 @@ class RequestLinkPanel extends React.Component {
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
 
-    if(this.props.isLoaded) Promise.all([
-        //this.state.gasCosts * this.state.gasPrice
-      this.getBalance(this.props.accounts[0]),
+    const currentAccount = this.props.accounts ? this.props.accounts[0] : '';
+
+    // Move this logic to the store - components does not want dependency of props.accounts[]
+    if (this.props.isLoaded) Promise.all([
+      //this.state.gasCosts * this.state.gasPrice
+      this.getBalance(currentAccount),
       this.getGasPrice(),
       this.estimateGasCosts(),
     ]).then(res => {
@@ -49,16 +52,16 @@ class RequestLinkPanel extends React.Component {
 
 
     /**
-      // TODO: Improve with web3 bignumber comparison?
-      if(!this.validateAccountBalance(balance.toString())){
+     // TODO: Improve with web3 bignumber comparison?
+     if(!this.validateAccountBalance(balance.toString())){
         this.setState({
           isFormSucces: false,
-          errorMessages: this.state.errorMessages.concat([`No sufficient funds in your wallet @ ${this.props.accounts[0]}`])
+          errorMessages: this.state.errorMessages.concat([`No sufficient funds in your wallet @ ${currentAccount}`])
         })
       };**/
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
   }
 
   validateInput = (input) => {
@@ -68,7 +71,7 @@ class RequestLinkPanel extends React.Component {
     var regex = new RegExp(expression);
 
     // If input is long enough
-    if(input && input.length > 5){
+    if (input && input.length > 5) {
       // Check if it's a valid url
       // https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
       if (input.match(regex)) {
@@ -87,7 +90,7 @@ class RequestLinkPanel extends React.Component {
     //console.log('FEE = ' , this.state.gasPrice  * this.state.gasCosts)
 
     // Min. fee of 0
-    if(this.state.gasPrice  * this.state.gasCosts > 0){
+    if (this.state.gasPrice * this.state.gasCosts > 0) {
       result = true;
     }
 
@@ -98,7 +101,7 @@ class RequestLinkPanel extends React.Component {
     let result = false;
 
     // Min. account balance of 0
-    if(balance.toNumber() > 0){
+    if (balance.toNumber() > 0) {
       result = true;
     }
 
@@ -112,11 +115,11 @@ class RequestLinkPanel extends React.Component {
     });
 
     // TODO: Improve this & return specific errorMessages for each validator
-    if(
+    if (
         this.validateInput(this.state.addNewLinkURL) &&
         this.validateFee() &&
         this.validateAccountBalance(this.state.accountBalance)
-    ){
+    ) {
       this.setState({
         isFormValid: true,
         errorMessages: []
@@ -135,7 +138,7 @@ class RequestLinkPanel extends React.Component {
         message: '',
         isCustomMessageAdded: false,
       })
-    }else{
+    } else {
       this.setState({
         isFormValid: false,
         errorMessages: ['That\'s not a valid URL.']
@@ -146,7 +149,7 @@ class RequestLinkPanel extends React.Component {
   getBalance = (account) => {
     return new Promise((resolve, reject) => {
       return this.props.web3.eth.getBalance(account, (err, res) => {
-        if(err) reject(err);
+        if (err) reject(err);
         resolve(res);
       })
     });
@@ -155,7 +158,7 @@ class RequestLinkPanel extends React.Component {
   getGasPrice = () => {
     return new Promise((resolve, reject) => {
       return this.props.web3.eth.getGasPrice((err, res) => {
-        if(err) reject(err);
+        if (err) reject(err);
         resolve(res);
       })
     });
@@ -163,15 +166,16 @@ class RequestLinkPanel extends React.Component {
 
   estimateGasCosts = () => {
     //console.log('Estimating gas costs to ...' , this.props.contract.address.toString());
+    const currentAccount = this.props.accounts ? this.props.accounts[0] : '';
 
     // TODO: Improve estimation w. reall address & data
     return new Promise((resolve, reject) => {
       this.props.web3.eth.estimateGas({
-        from: this.props.accounts[0].toString(),
+        from: currentAccount.toString(),
         to: "0xc4abd0339eb8d57087278718986382264244252f",
         data: "0xc6888fa10000000000000000000000000000000000000000000000000000000000000003"
       }, (err, res) => {
-        if(err) reject(err)
+        if (err) reject(err)
         resolve(res);
       });
     });
@@ -200,20 +204,22 @@ class RequestLinkPanel extends React.Component {
 
   render() {
 
-    if(!this.props.isLoaded || !this.props.accounts) return <Loader inverted active size='large'>Loading Contract.</Loader>;
+    if (!this.props.isLoaded || !this.props.accounts) return <Loader inverted active size='large'>Loading
+      Contract.</Loader>;
+    
+    const CONTRACT_ADDRESS = this.props.contract.address ? this.props.contract.address.toString() : '';
+    const currentAccount = this.props.accounts.length > 0 ? this.props.accounts[0] : '';
 
-
-    const CONTRACT_ADDRESS = this.props.contract.address.toString();
-    return  (
+    return (
         <RequestLinkPanelContainer
-                     isOpen={this.props.isOpen}
-                     title="Show Your Video To The World!"
-                     onClose={this.onCloseClick}
-                     onConfirm={() => this.onSubmitForm()}>
+            isOpen={this.props.isOpen}
+            title="Show Your Video To The World!"
+            onClose={this.onCloseClick}
+            onConfirm={() => this.onSubmitForm()}>
           <div>
 
             <Row>
-              <Column style={{ flex: 3}}>
+              <Column style={{ flex: 3 }}>
                 <h2>How does it work?</h2>
                 <p>
                   Show your video to the world by sending an Ethereum transaction.<br />
@@ -224,7 +230,7 @@ class RequestLinkPanel extends React.Component {
                 </p>
               </Column>
 
-              <Column style={{ flex: 2}}>
+              <Column style={{ flex: 2 }}>
                 <h2>What do we support?</h2>
 
                 Wallets
@@ -242,106 +248,113 @@ class RequestLinkPanel extends React.Component {
               </Column>
             </Row>
 
-        <Form success={this.state.errorMessages.length === 0 && this.state.isFormValid}
-          error={this.state.errorMessages.length > 0}>
-          <h2>Send a transaction</h2>
-          <Form.Field>
-            <label htmlFor="userAddress">FROM</label>
+            <Form success={this.state.errorMessages.length === 0 && this.state.isFormValid}
+                  error={this.state.errorMessages.length > 0}>
+              <h2>Send a transaction</h2>
+              <Form.Field>
+                <label htmlFor="userAddress">FROM</label>
 
-            <Label style={{
-              display: 'flex',
-              flex: 1,
-              alignItems: 'center',
-              justifyContent: 'center'
-            }}
-                   as='a'
-                   onClick={() => window.open(`https://etherscan.io/address/${this.props.accounts[0]}`) }
-                   alt="Click to show your address on Etherscan"
-                   title="Click to show your address on Etherscan">
-              <Blocky seed={this.props.accounts[0]} />
+                <Label style={{
+                  display: 'flex',
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }}
+                       as='a'
+                       onClick={() => window.open(`https://etherscan.io/address/${currentAccount}`) }
+                       alt="Click to show your address on Etherscan"
+                       title="Click to show your address on Etherscan">
+                  <Blocky seed={currentAccount} />
 
-              <Column style={{ marginLeft: 25 }}>
-                Your Wallet Address
-                <strong>{this.props.accounts[0]}</strong>
-              </Column>
-            </Label>
-          </Form.Field>
+                  <Column style={{ marginLeft: 25 }}>
+                    {currentAccount === '' ? 'Please unlock your wallet.' : 'Your Wallet Address'}
+                    <strong>{currentAccount}</strong>
+                  </Column>
+                </Label>
+              </Form.Field>
 
-            <Form.Field>
-              <label htmlFor="contractAddress">TO</label>
+              <Form.Field>
+                <label htmlFor="contractAddress">TO</label>
 
-              <Label style={{
-                display: 'flex',
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-              }}
-                     as='a'
-                     onClick={() => window.open(`https://etherscan.io/address/${CONTRACT_ADDRESS}`) }
-                     alt="Click to see contract on Etherscan"
-                     title="Click to show contract on Etherscan">
-                <Blocky seed={CONTRACT_ADDRESS} />
+                <Label style={{
+                  display: 'flex',
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }}
+                       as='a'
+                       onClick={() => window.open(`https://etherscan.io/address/${CONTRACT_ADDRESS}`) }
+                       alt="Click to see contract on Etherscan"
+                       title="Click to show contract on Etherscan">
+                  <Blocky seed={CONTRACT_ADDRESS} />
 
-                <Column style={{ marginLeft: 25 }}>
-                  ETH.TV Contract address
-                  <strong>{CONTRACT_ADDRESS}</strong>
-                </Column>
-              </Label>
-            </Form.Field>
+                  <Column style={{ marginLeft: 25 }}>
+                    ETH.TV Contract address
+                    <strong>{CONTRACT_ADDRESS}</strong>
+                  </Column>
+                </Label>
+              </Form.Field>
 
-            <Form.Field required>
-              <label htmlFor="input" style={{ textTransform: 'capitalize'}}>VIDEO URL</label>
-              <Input size="big" name="input" type="text" placeholder="https://www.youtube.com/watch?v=jlNvOWDfMYo" onChange={(evt) => this.setState({ addNewLinkURL: evt.target.value })} />
-            </Form.Field>
+              <Form.Field required>
+                <label htmlFor="input" style={{ textTransform: 'capitalize' }}>VIDEO URL</label>
+                <Input size="big" name="input" type="text" placeholder="https://www.youtube.com/watch?v=jlNvOWDfMYo"
+                       onChange={(evt) => this.setState({ addNewLinkURL: evt.target.value })} />
+              </Form.Field>
 
-           { this.state.isCustomMessageAdded && <Form.Field>
-              <label htmlFor="message">MESSAGE</label>
-              <TextArea placeholder="Add your message" id="message" name="message" value={this.state.message} onChange={(evt) => this.setState({ message: evt.target.value })} />
-            </Form.Field>}
+              { this.state.isCustomMessageAdded && <Form.Field>
+                <label htmlFor="message">MESSAGE</label>
+                <TextArea placeholder="Add your message" id="message" name="message" value={this.state.message}
+                          onChange={(evt) => this.setState({ message: evt.target.value })} />
+              </Form.Field>}
 
-          <Form.Checkbox label='I want to add a custom message for 0.5 ETH.' checked={this.state.isCustomMessageAdded} onClick={() => {
-            if(!this.state.isCustomMessageAdded){
-              this.setState({ inputCost: this.props.web3.toWei(0.5, 'ether')})
-            }else{
-              this.setState({ inputCost: this.props.web3.toWei(0, 'ether')})
-            }
+              <Form.Checkbox label='I want to add a custom message for 0.5 ETH.'
+                             checked={this.state.isCustomMessageAdded} onClick={() => {
+                if (!this.state.isCustomMessageAdded) {
+                  this.setState({ inputCost: this.props.web3.toWei(0.5, 'ether') })
+                } else {
+                  this.setState({ inputCost: this.props.web3.toWei(0, 'ether') })
+                }
 
-            this.setState({ isCustomMessageAdded: !this.state.isCustomMessageAdded })}
-          } />
-          <Form.Group widths="equal" style={{ display: 'flex', flexDirection: 'row'}}>
-            <Form.Field>
-              <label htmlFor="inputCost">COST (in wei)</label>
-              <Row style={{ alignItems: 'center'}}>
-                <input type="number" disabled min={0} placeholder={this.state.inputCost} id="inputCost" name="inputCost" value={this.state.inputCost} />
-                wei
-              </Row>
-              <span><small>Your balance: {this.state.accountBalance.toString()} wei available</small></span>
-            </Form.Field>
+                this.setState({ isCustomMessageAdded: !this.state.isCustomMessageAdded })
+              }
+              } />
+              <Form.Group widths="equal" style={{ display: 'flex', flexDirection: 'row' }}>
+                <Form.Field>
+                  <label htmlFor="inputCost">COST (in wei)</label>
+                  <Row style={{ alignItems: 'center' }}>
+                    <input type="number" disabled min={0} placeholder={this.state.inputCost} id="inputCost"
+                           name="inputCost" value={this.state.inputCost} />
+                    wei
+                  </Row>
+                  <span><small>Your balance: {this.state.accountBalance.toString()} wei available</small></span>
+                </Form.Field>
 
-            <Form.Field>
-              <label htmlFor="inputFee">FEE (in wei)</label>
-              <Row style={{ alignItems: 'center'}}>
-                <input disabled type="number" min={0} placeholder={this.state.gasCosts * this.state.gasPrice} id="inputFee" name="inputFee" value={this.state.gasCosts * this.state.gasPrice} onChange={(evt) => this.setState({ fee: evt.target.value })} />
-                wei
-              </Row>
-              <span><small>Gas Price: {this.state.gasPrice.toString()} wei</small></span>
-            </Form.Field>
-          </Form.Group>
+                <Form.Field>
+                  <label htmlFor="inputFee">FEE (in wei)</label>
+                  <Row style={{ alignItems: 'center' }}>
+                    <input disabled type="number" min={0} placeholder={this.state.gasCosts * this.state.gasPrice}
+                           id="inputFee" name="inputFee" value={this.state.gasCosts * this.state.gasPrice}
+                           onChange={(evt) => this.setState({ fee: evt.target.value })} />
+                    wei
+                  </Row>
+                  <span><small>Gas Price: {this.state.gasPrice.toString()} wei</small></span>
+                </Form.Field>
+              </Form.Group>
 
-          <Message
-              error
-              header='An error occured'
-              list={this.state.errorMessages}
-          />
-        </Form>
-      </div>
+              <Message
+                  error
+                  header='An error occured'
+                  list={this.state.errorMessages}
+              />
+            </Form>
+          </div>
         </RequestLinkPanelContainer>
     );
   }
 }
 
 RequestLinkPanel.defaultProps = {};
-RequestLinkPanel.propTypes    = {
+RequestLinkPanel.propTypes = {
   contract: PropTypes.object,
   isOpen: PropTypes.bool.isRequired,
   accounts: React.PropTypes.array,
