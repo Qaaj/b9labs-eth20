9e1661b903bcddd7661f3594e4bc0974c6a29a11
Jan Jorissen <grasmaaier45@gmail.com>
2018-07-08 14:35:23 +0200

    New migrations

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ const CloseButton = styled.div`
 
 class App extends Component {
 
-  //state = { component: null };
-  state = { component: 'eth.tv' };
+  state = { component: null };
+  // state = { component: 'eth.tv' };
 
   constructor(props) {
     super(props);
